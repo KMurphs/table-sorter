eb96cb1c54c9391847e9b03cd8f94b08b3e82e02
stephane kibonge <stephane.kibonge@etion.co.za>
2020-12-27 12:02:14 +0200

    Added tailwindcss

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Table Sorter
+
+## Definition
+
+This app is an exercise involving different sorting algorithms on a table ui components written in React, Typescript and TailwindCSS.
+
---
+++ scripts/setup.bat
@@ -0,0 +1,16 @@
+
+npx create-react-app table-sorting --template typescript
+
+curl https://www.toptal.com/developers/gitignore/api/react,code > .gitignore
+git add .gitignore
+git commit -m "Initial Commit"
+
+
+npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+npm install @craco/craco
+
+npm uninstall tailwindcss postcss autoprefixer
+npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+
+npm uninstall tailwindcss @tailwindcss/postcss7-compat
+npm install tailwindcss@latest postcss@latest autoprefixer@latest
