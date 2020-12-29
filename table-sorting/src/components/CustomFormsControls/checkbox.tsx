e321a332edd5f18c227077e60920eeff48bdda9b
stephane kibonge <stephane.kibonge@etion.co.za>
2020-12-29 12:04:02 +0200

    Added header to Table Sorter screen

---
+++ table-sorting/src/components/CustomFormsControls/checkbox.tsx
@@ -0,0 +1,77 @@
+import "./checkbox.css";
+
+type Props = {
+  type?: number
+  onText?: string
+  offText?: string
+  constantText?: string
+  extraClasses?: string
+  value: boolean
+  onChange: (newVal: boolean)=>void
+}
+type CheckoxProps = {
+  onText?: string
+  offText?: string
+  constantText?: string
+  extraClasses?: string
+  value: boolean
+  onChange: (newVal: boolean)=>void
+}
+export default function Checkbox({type, ...props}: Props) {
+  if(type === 1) return <Checkbox1 {...props}/> 
+  return <Checkbox0 {...props}/> 
+}
+(Checkbox as React.FC).defaultProps = {
+  type: 0,
+  onText: "",
+  offText: "",
+  constantText: "",
+  extraClasses: "",
+  value: false,
+}
+
+
+
+function Checkbox0({value, onChange, onText, offText, constantText, extraClasses}: CheckoxProps) {
+
+  return (
+    <label htmlFor="custom-checkbox" className={`custom-checkbox__container custom-checkbox__container-0 ${extraClasses}`}>
+       <input type="checkbox" id="custom-checkbox" name="custom-checkbox" checked={value} onChange={evt=>onChange(evt.target.checked)} />
+       <div className="custom-checkbox__background">
+          <span className="custom-checkbox__thumb"></span>
+       </div>
+       <p className="custom-checkbox__text">{constantText}<span>{offText}</span> <span>{onText}</span></p>     
+    </label>
+  )
+}
+
+function Checkbox1({value, onChange, onText, offText, constantText, extraClasses}: CheckoxProps) {
+
+  return (
+    <label htmlFor="custom-checkbox" className={`custom-checkbox__container custom-checkbox__container-1 ${extraClasses}`}>
+       <input type="checkbox" id="custom-checkbox" name="custom-checkbox" checked={value} onChange={evt=>onChange(evt.target.checked)} />
+       <div className="custom-checkbox__background">
+        <span className="custom-checkbox__thumb"></span>
+       </div>
+       <p className="custom-checkbox__text">{constantText}<span>{offText}</span> <span>{onText}</span></p>     
+    </label>
+  )
+}
+
+
+
+
+
+
+function CheckboxSource() {
+
+  return (
+    <label htmlFor="custom-checkbox" className="custom-checkbox-container">
+       <input type="checkbox" value="1" id="custom-checkbox" name="" />
+       <div className="custom-checkbox-background">
+          <span></span>
+       </div>
+       <p>Checkbox <span>Deselected</span> <span>Selected</span></p>     
+    </label>
+  )
+}
--- table-sorting/src/components/TableSorter/index.tsx
+++ table-sorting/src/components/TableSorter/index.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+import Checkbox from '../CustomFormsControls/checkbox';
 import './index.css';
 
 
 
 export default function TableSorter() {
 
+  const [checkbox, setCheckbox] = useState<boolean>(true)
+
   return (
-    <h1>TableSorter</h1>
+    <div className="table-sorter">
+      <header className="table-sorter__header grid p-4 lg:flex lg:justify-between lg:flex-row lg:items-end">
+
+        <section className="flex justify-between items-center md:inline-block relative z-10">
+          <h1 className="text-2xl lg:text-4xl">Table Sorter</h1>
+          <label htmlFor="app-menu-control--hidden" id="app-menu-control" className="text-2xl flex md:hidden">
+            <i className="fas fa-bars"></i>
+          </label>
+        </section>
+
+
+        <input type="checkbox" id="app-menu-control--hidden" className="hidden"/>
+        <section className="app-menu pt-4 lg:pt-0 flex text-lg flex-col md:flex-row ">
+          <div className="flex items-center lg:mr-8 flex-col md:flex-row">
+            <button className=" py-2 btn btn--link">Reset</button>
+            <span className="p-4 py-2"><Checkbox type={1} value={checkbox} onChange={setCheckbox} constantText={"Direction: "} onText={"Reverse"} offText={"Normal"} extraClasses={"with-muted-p"}/></span>
+          </div>
+          <ul className="sort-algorithms flex items-stretch flex-col md:flex-row rounded overflow-hidden">
+            <li><input className="hidden sort-algorithm" type="radio" name="sort-algorithm" value="regular-sort" id="regular-sort" /><label className="p-4 py-2 text-center" htmlFor="regular-sort">Regular Sort</label></li>
+            <li><input className="hidden sort-algorithm" type="radio" name="sort-algorithm" value="bubble-sort" id="bubble-sort" /><label className="p-4 py-2 text-center" htmlFor="bubble-sort">Bubble Sort</label></li>
+            <li><input className="hidden sort-algorithm" type="radio" name="sort-algorithm" value="merge-sort" id="merge-sort" /><label className="p-4 py-2 text-center" htmlFor="merge-sort">Merge Sort</label></li>
+            <li><input className="hidden sort-algorithm" type="radio" name="sort-algorithm" value="insertion-sort" id="insertion-sort" /><label className="p-4 py-2 text-center" htmlFor="insertion-sort">Insertion Sort</label></li>
+            <li><input className="hidden sort-algorithm" type="radio" name="sort-algorithm" value="quartile-sort" id="quartile-sort" /><label className="p-4 py-2 text-center" htmlFor="quartile-sort">Quartile Sort</label></li>
+          </ul>
+        </section>
+      </header>
+      <main>Table</main>
+    </div>
+    
   )
 
 }
