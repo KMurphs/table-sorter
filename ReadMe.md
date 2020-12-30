09497804d7422f1de10a01a138fba70bb02e3e89
stephane kibonge <stephane.kibonge@etion.co.za>
2020-12-30 15:31:54 +0200

    Fixed default sorting direction

--- ReadMe.md
+++ ReadMe.md
@@ -4,3 +4,4 @@
 
 This app is an exercise involving different sorting algorithms on a table ui components written in React, Typescript and TailwindCSS.
 
+## 
--- scripts/setup.bat
+++ scripts/setup.bat
@@ -9,8 +9,11 @@ git commit -m "Initial Commit"
 npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
 npm install @craco/craco
 
-npm uninstall tailwindcss postcss autoprefixer
-npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+@REM npm uninstall tailwindcss postcss autoprefixer
+@REM npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
 
-npm uninstall tailwindcss @tailwindcss/postcss7-compat
-npm install tailwindcss@latest postcss@latest autoprefixer@latest
+@REM npm uninstall tailwindcss @tailwindcss/postcss7-compat
+@REM npm install tailwindcss@latest postcss@latest autoprefixer@latest
+
+npm install react-router-dom
+npm i --save-dev @types/react-router-dom
