31585f419903e0dc3c4492b7ee8b84e19c950e9a
stephane kibonge <stephane.kibonge@etion.co.za>
2021-01-02 15:20:05 +0200

    Released current automated build to Github page

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,19 +6,19 @@ CD ..\table-sorting
 @REM npm run build
 
 
-@REM Delete current release folder content
+ECHO Delete current release folder content
 DEL /F/Q/S ..\docs\app\*.* > NUL
 DEL /F/Q/S ..\docs\*.* > NUL
 RMDIR ..\docs\app
 
-@REM Copy Github page files
+ECHO Copy Github page files
 @REM Github Pages expect the entry point to be index.md or readme.md 
 ROBOCOPY ..\github-pages ..\docs\ /MIR /NFL /NDL 
 COPY ..\readme.md ..\docs
 COPY ..\build\favicon.ico ..\docs
 
 
-@REM Copy release files
+ECHO Copy release files
 @REM https://stackoverflow.com/a/7487697/9034699
 MKDIR ..\docs\app
 ROBOCOPY build ..\docs\app /MIR /NFL /NDL 
@@ -27,11 +27,11 @@ ROBOCOPY build ..\docs\app /MIR /NFL /NDL
 
 
 
-@REM Push to origin
+ECHO Push to origin
 CD ..
 git add *
 git commit -m "Released current automated build to Github page"
 git push 
 
-@REM Return to original directory
+ECHO Return to original directory
 CD .\table-sorting
